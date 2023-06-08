4d82588246a1639efe3d71c387c08b1bb044d68d
BarrySong <524000659@qq.com>
2023-06-08 10:17:52 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# pre set-up dependencies
+
+- [vite](https://vitejs.dev/)
+- [semi-design (semi-design / semi-icon / semi-illustrations)](https://semi.design/zh-CN/)
+- [tailwinds](https://tailwindcss.com/docs/guides/vite)
+- [react-router](https://reactrouter.com/en/main)
+- [ahooks](https://ahooks.js.org/)
+- [react-query](https://tanstack.com/query/v3)
+- [react-i18next](https://github.com/i18next/react-i18next)
+- less
+
+# develop set-up
+
+install dependencies
+
+```bash
+pnpm i
+
+npm i
+
+yarn
+```
+
+start project
+```bash
+pnpm dev
+
+npm dev
+
+yarn dev
+```
---
+++ src/main.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
+import Home from "./pages/Home/index.tsx";
+import About from "./pages/About/index.tsx";
+import "@douyinfe/semi-ui/dist/css/semi.min.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // loader: rootLoader,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
